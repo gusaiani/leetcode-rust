ce4953a2d91d00e2afe809d451e8eb5d876511bf
Gustavo Saiani <gustavo@poe.ma>
2026-05-20 13:09:44 -0300

    Fix clippy ptr_arg on reverse_string signature

--- src/reverse_string.rs
+++ src/reverse_string.rs
@@ -7,18 +7,7 @@
 //! You must do this by modifying the input array in-place with O(1) extra
 //! memory.
 
-/*
-pub fn reverse_string(s: &mut Vec<char>) {
-    let (mut i, mut j) = (0, s.len().saturating_sub(1));
-    while i < j {
-        s.swap(i, j);
-        i += 1;
-        j -= 1;
-    }
-}
-*/
-
-pub fn reverse_string(s: &mut Vec<char>) {
+pub fn reverse_string(s: &mut [char]) {
     let n = s.len();
     for i in 0..n / 2 {
         s.swap(i, n - 1 - i);
